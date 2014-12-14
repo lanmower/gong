4cd50c00d9b0ac721e96b003df28df07fd1c1e40
James Vos <lanmower@50-af-73-65-d0-5a.dummy.porta.siemens.net>
2014-12-14 18:10:29 +0200

    modified:   protected/modules/pga/widgets/GScoreRankingPro.php

--- protected/modules/pga/widgets/GScoreRankingPro.php
+++ protected/modules/pga/widgets/GScoreRankingPro.php
@@ -2,12 +2,12 @@
 
 class GScoreRankingPro extends GTag {
 
-    public function init() {
-        $this->script = '
+	public function init() {
+		$this->script = '
 						setTimeout("location.reload(true);", 240000);
 					';
-        if (isset($_GET ['scroll']))
-            $this->script .= '$("html, body").animate({ scrollTop: $(document).height() }, 120000, "linear");
+		if (isset($_GET ['scroll']))
+		$this->script .= '$("html, body").animate({ scrollTop: $(document).height() }, 120000, "linear");
     setTimeout(function() {
        $("html, body").animate({scrollTop:0}, 120000, "linear"); 
     },120000);
@@ -19,18 +19,48 @@ class GScoreRankingPro extends GTag {
 
     },240000);
 ';
-        parent::init();
-    }
+		parent::init();
+	}
 
-    public function run() {
-        $players = GSubmission::forForm('Player')->findAll();
-        foreach($players as $player) {
-        	echo $player->name.":";
-        	$rounds = ScoreTools::playerScore(array($player));
-        	echo $rounds['total']['player'];
-        }
-        $data = array();
-    }
+	public function run() {
+		$proGame = GSubmission::forForm("Game")->find('name = :name', array(":name"=>'Professional'));
+		$players = GSubmission::forForm('Player')->findAll('game = :game', array(":game"=>$proGame->id));
+		$data = array();
+		foreach($players as $player) {
+			$rounds = ScoreTools::playerScore(array($player));
+			$data[] = array('player'=>$player, 'total'=>$rounds['total']['player']);
+		}
+
+		usort ( $data, function ($a, $b) {
+			return $a ['total'] > $b ['total'];
+		} );
+
+		echo "<table class='table'>";
+		echo "<tr>";
+		echo "<th class='th1'>Position</th>";
+		echo "<th class='th2'>Player</th>";
+		echo "<th class='th4'>Score</th>";
+		echo "</tr>";
+		$pos = 0;
+		foreach ( $data as $playerData ) {
+			$player = $playerData['player'];
+			$total = $playerData['total'];
+			echo "<tr>";
+			echo "<td>" . ++ $pos . "</td>";
+			echo "<td>";
+			if (isset ( $player->country )) {
+				echo CHtml::image ( str_replace ( 'protected/data/form_uploads/GSubmission/', '/protected/data/form_uploads/GSubmission/', $player->country->flag ) );
+			}
+			echo $player ['name'];
+			echo "</td>";
+			echo CHtml::tag ( 'td', array (), $total );
+
+			echo "</tr>";
+		}
+		echo "</tr>";
+		echo "</table>";
+
+	}
 
 }
 
