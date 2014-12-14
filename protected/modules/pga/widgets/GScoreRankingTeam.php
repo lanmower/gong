f8ca24b15978e9d1faa9c4c25815619b8df1c9e9
James Vos <lanmower@50-af-73-65-d0-5a.dummy.porta.siemens.net>
2014-12-14 20:50:11 +0200

    modified:   protected/modules/pga/widgets/GScoreRankingTeam.php

--- protected/modules/pga/widgets/GScoreRankingTeam.php
+++ protected/modules/pga/widgets/GScoreRankingTeam.php
@@ -25,17 +25,56 @@ class GScoreRankingTeam extends GTag {
     public function run() {
     	$d = isset($_GET['debug']);
         $teams = GSubmission::forForm('Team')->findAll();
+    	$data = array();
         foreach($teams as $team) {
-        	echo $team->name.":";
         	$rounds = ScoreTools::playerScore($team->players);
-        	if($d) CVarDumper::dump($rounds, 4, true);
-        	echo $rounds['total']['team'];
-        	foreach($team->players as $player) {
-	        	echo $player->name.':'.$rounds['total']['player'][$player->id];
-        	}
-        	echo "<br/>";
+        	$players = array();
+        	foreach($rounds['total']['player'] as $playerId => $playerData) {
+				$player = null;
+				foreach($team->players as $tmpPlayer) {
+					if($playerId = $tmpPlayer->id) $players[$playerId] = array('player'=>$tmpPlayer, 'total'=>$playerData);
+				}
+			}
+			usort ( $data, function ($a, $b) {
+				return $a ['total'] > $b ['total'];
+			} );
+			$data[] = array('team'=>$team, 'total'=>$rounds['total']['team'], 'players'=>$players);
         }
-        $data = array();
+		usort ( $data, function ($a, $b) {
+			return $a ['total'] > $b ['total'];
+		} );
+		
+		echo "<table class='table'>";
+		echo "<tr>";
+		echo "<th>POS</th>";
+		echo "<th>Team</th>";
+		echo "<th>Players</th>";
+		echo "<th>Total</th>";
+		echo "</tr>";
+		$pos = 0;
+		foreach ( $data as $team ) {
+			if ($team ['total'] == 0)
+				continue;
+			echo "<tr>";
+			echo "<td>" . ++ $pos . "</td>";
+			echo "<td>" . $team ['team']->name . "</td>";
+			echo "<td>";
+			foreach ( $team ['players'] as $player ) {
+				echo CHtml::openTag ( 'div', array (
+						'class' => 'scorelistplayer' 
+				) );
+				if (isset ( $player->country )) {
+					echo CHtml::image ( str_replace ( 'protected/data/form_uploads/GSubmission/', '/protected/data/form_uploads/GSubmission//', $player->country->flag ) );
+				}
+				echo $player['player']->name;
+				echo CHtml::closeTag ( 'div' );
+			}
+			echo "</td>";
+			echo CHtml::tag ( 'td', array (), $team ['total'] );
+		}
+		echo "</tr>";
+		echo "</table>";
+				
     }
 
 }
