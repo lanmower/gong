800dabdade42121ea7d059e6b68a178036c2fe18
James Vos <almagestfraternite@gmail.com>
2015-01-12 12:52:39 +0200

    fixie

--- protected/modules/gong/modules/form/widgets/GHash.php
+++ protected/modules/gong/modules/form/widgets/GHash.php
@@ -3,11 +3,11 @@ class GHash extends GField {
 	public function init() {
 	}
 	public function run() {
-		echo PseudoCrypt::hash($this->submission->id);
+		if(isset($this->submission)) echo PseudoCrypt::hash($this->submission->id);
 		parent::run ();
 	}
 	public function getCell($value) {
-		return PseudoCrypt::hash($this->submission->id);
+		if(isset($this->submission)) return PseudoCrypt::hash($this->submission->id);
 	}
 	public function getGridColumns() {
 		return array (
