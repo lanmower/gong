8e68cfe61f8a816bf28520b8faa1978c32e41fe7
James Vos <lanmower@50-af-73-65-d0-5a.dummy.porta.siemens.net>
2015-01-26 10:37:11 +0200

    modified:   protected/modules/pga/controllers/ScoreController.php

--- protected/modules/pga/controllers/ScoreController.php
+++ protected/modules/pga/controllers/ScoreController.php
@@ -42,6 +42,7 @@ class ScoreController extends GController {
 					
 					if($holeCount > $tHoleCount) $dontStore = true;
 				}
+				echo $lastPlayer.'\n';
 				die($nextPlayer);
 
 				if(isset($player)) {
